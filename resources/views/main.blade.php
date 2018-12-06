f02550b42d21762ed8efd2382ee5c2b3974475bc
NingqianJia <ricky.jia2018@gmail.com>
2018-12-06 03:33:36 -0600

    finished index

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ningqianjia
- * Date: 11/9/18
- * Time: 4:45 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+@include('partials._head')
+<body>
+{{--Default boostrap navbar--}}
+    @include('partials._nav')
+
+    <div class="container">
+        @include('partials._messages')
+        @yield('content')
+        @include('partials._footer')
+    </div> <!-- end of .container -->
+    @include('partials._javascript')
+    @yield('script')
+</body>
+</html>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,7 +1,2 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ningqianjia
- * Date: 11/11/18
- * Time: 4:49 PM
- */
+<hr>
+<p class="text-center">Copyright RickyJia - All Rights Reserved</p>
