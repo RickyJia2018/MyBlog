cbe17e3ddd5182da63c0b95824a77703da2b6daa
NingqianJia <ricky.jia2018@gmail.com>
2019-03-02 07:04:38 -0600

    a

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -28,7 +28,7 @@
         </div>
         <div class="col-md-3 col-md-offset-1" >
             <h2>Sidebar</h2>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, deleniti, maxime! Eum facere odio rem tempore temporibus! Dignissimos esse et ipsam suscipit voluptas voluptatum? Cupiditate fuga libero quae reprehenderit saepe.
+            {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, deleniti, maxime! Eum facere odio rem tempore temporibus! Dignissimos esse et ipsam suscipit voluptas voluptatum? Cupiditate fuga libero quae reprehenderit saepe.--}}
         </div>
     </div>
 
